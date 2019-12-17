e3bce613317628d95b9160a8b0f5e3eec78dd821
Stayfi <stayfi@mail.com>
2019-12-17 09:49:40 +0100

    OSX tests

--- scripts/copy_assets_views_prod.sh
+++ scripts/copy_assets_views_prod.sh
@@ -2,7 +2,7 @@
 mkdir -p ./dist/app
 cd src/app
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  ditto `find . -name *.njk` ../../dist/app/
+  rsync -R `find . -name *.njk` ../../dist/app/
 else
   cp --parents `find . -name *.njk` ../../dist/app/
 fi
